db8519d61995c38f28ad50a837d6ee0499628833
bdoze90 <bjmendo@gmail.com>
2018-07-27 17:03:23 -0400

    Fixed most of the indexing errors

--- SeqMutator.py
+++ SeqMutator.py
@@ -9,59 +9,58 @@ from RosettaSub import RosettaSingleProcess
 class SeqMutator:
 
     def __init__(self, base):
-        self.seq_lists = ["rna_seqs_copy.txt", "dna_seqs_copy.txt"]
+        self.seq_lists = {'r': "rna_seqs_copy.txt", 'd': "dna_seqs_copy.txt"}
         self.base_dir = base  # base_pdb should be set to the pdb that is missing the respective DNA or RNA
         self.new_seqs = list()
 
-        self.cs_dict = {"4UN3/ChainA": (0, 81, '', ''),
-                        "4UN3/ChainC": (5, 'n', '',''),
-                        "4UN3/ChainD": (17, 'n', '','TGGTATTG'),
-                        "4UN4/ChainA": (0, 81, '',''),
-                        "4UN4/ChainC": (0, 12, 'rc',''),
-                        "4UN4/ChainD": (18, 'n', '','TGGATTG'),
-                        "4UN4/ChainE": (13, 'n', '',''),
-                        "4UN5/ChainA": (0, 81, '',''),
-                        "4UN5/ChainC": (0, 12, 'rc',''),
-                        "4UN5/ChainD": (18, 'n', '','TGGATTG'),
-                        "4UN5/ChainE": (13, 'n', 'rc',''),
-                        "5FQ5/ChainA": (0, 81, '',''),
-                        "5FQ5/ChainC": (0, 12, 'rc',''),
-                        "5FQ5/ChainD": (18, 'n', '','TGGATTG'),
-                        "5FQ5/ChainE": (13, 'n', 'rc',''),
-                        "4OO8ABC/ChainB": (0, 'n', '',''),
-                        "4OO8ABC/ChainC": (0, 'n', 'rc','')
+        self.cs_dict = {"4UN3/ChainA": ('r', 0, 81, '', ''),
+                        "4UN3/ChainC": ('d', 0, 'n', 'rc','TGGTATTG'),
+                        "4UN3/ChainD": ('d', 17, 'n', '','TGGTATTG'),
+                        "4UN4/ChainA": ('r', 0, 81, '',''),
+                        "4UN4/ChainC": ('d', 17, 'n', 'rc','TGGTATTG'),
+                        "4UN4/ChainD": ('d', 17, 'n', '','TGGTATTG'),
+                        "4UN4/ChainE": ('d', 13, 'n', '',''),
+                        "4UN5/ChainA": ('r', 0, 81, '',''),
+                        "4UN5/ChainC": ('d', 17, 'n', 'rc','TGGTATTG'),
+                        "4UN5/ChainD": ('d', 17, 'n', '','TGGTATTG'),
+                        "4UN5/ChainE": ('d', 0, 17, 'rc',''),
+                        "5FQ5/ChainA": ('r', 0, 81, '',''),
+                        "5FQ5/ChainC": ('d', 17, 'n', 'rc','TGGTATTG'),
+                        "5FQ5/ChainD": ('d', 17, 'n', '','TGGTATTG'),
+                        "5FQ5/ChainE": ('d', 0, 17, 'rc',''),
+                        "4OO8ABC/ChainB": ('r', 0, 'n', '',''),
+                        "4OO8ABC/ChainC": ('r', 0, 'n', 'rc','')
                         }
 
         for chain in self.cs_dict:
             self.read_in_seqs(chain)  # STEP 1: get the RNA/DNA mutation sequences and put them in the SOLO folder
-            self.change_chain_name()
 
     def read_in_seqs(self, chain_name):
         base_pdb = self.base_dir + chain_name + ".pdb"
         output_directory = self.base_dir + chain_name + "_MUT" + "/"
-        for seq_file in self.seq_lists:
-            f = open(self.base_dir + seq_file)
-            for line in f:
-                s = line[:-1].split("\t")  # index 0: sequence_id; index 1: sequence for rosetta
-                ix = self.cs_dict[chain_name]
-                # check for addendum:
-                qs = s[1] + ix[3]
-                # check if the 'n' exists then just use the first index:
-                if ix[1] == 'n':
-                    seq = qs[ix[0]:]
-                    # check for revcomness:
-                    if ix[2] == 'rc':
-                        seq = self.revcom(seq)
-                else:
-                    seq = qs[ix[0]:ix[1]]  # grabs second index and the length of the sequence for the structure
-                    if ix[2] == 'rc':
-                        seq = self.revcom(seq)
-                print(seq)
-                rr = RosettaSingleProcess("rna_thread.default.macosclangrelease")
-                rr.set_inputs(
-                    ["-s", base_pdb, "-seq", seq.lower(), "-o", output_directory + s[0] + ".pdb"])
-                rr.run_process()
-            f.close()
+        seq_file = self.seq_lists[self.cs_dict[chain_name][0]]
+        f = open(self.base_dir + seq_file)
+        for line in f:
+            s = line[:-1].split("\t")  # index 0: sequence_id; index 1: sequence for rosetta
+            ix = self.cs_dict[chain_name]
+            # check for addendum:
+            qs = s[1] + ix[4]
+            # check if the 'n' exists then just use the first index:
+            if ix[2] == 'n':
+                seq = qs[ix[1]:]
+                # check for revcomness:
+                if ix[3] == 'rc':
+                    seq = self.revcom(seq)
+            else:
+                seq = qs[ix[1]:ix[2]]  # grabs second index and the length of the sequence for the structure
+                if ix[3] == 'rc':
+                    seq = self.revcom(seq)
+            print(seq)
+            rr = RosettaSingleProcess("rna_thread.default.macosclangrelease")
+            rr.set_inputs(
+                ["-s", base_pdb, "-seq", seq.lower(), "-o", output_directory + s[0] + ".pdb"])
+            rr.run_process()
+        f.close()
         self.change_chain_name(output_directory)
 
 
