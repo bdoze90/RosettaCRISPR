869d46c8c84f155e76ca2d7c4f36e3c2bd6fe385
bdoze90 <bjmendo@gmail.com>
2019-05-13 18:15:16 -0400

    Beginning of trimming analysis

--- Analysis/Trimming.py
+++ Analysis/Trimming.py
@@ -1,1 +1,13 @@
 """Creates a single file out of the truncated pdbs, deleting all information but the relevant RNA/DNA scores."""
+
+Structure = "4UN4"
+Ensemble = 1
+
+# Iterate over all the off-target directories in the ensemble off target directory:
+directory = "/Volumes/Seagate_Drive/RosettaCRISPR/" + Structure + "/Ensemble_" + str(i)
+fullmutdir = directory
+for file in fullmutdir:
+    if file.endswith(".pdb"):
+        f = open(file)
+
+        f.close()
