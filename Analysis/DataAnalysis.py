2a3928c13f9b8758fc57723013232996c9196f2a
bdoze90 <bjmendo@gmail.com>
2019-05-24 18:54:46 -0400

    Experimental value input

--- Analysis/DataAnalysis.py
+++ Analysis/DataAnalysis.py
@@ -11,12 +11,23 @@ class Analysis:
         self.basedirectory = base_dir
         self.struct = structure
 
+        self.experimental_dict = dict()
+
+        # Get the score correlations
+
+    def get_scores(self):
+        f = open(self.basedirectory + "HsuScores.txt")
+        for line in f:
+            expcor = line[:-1].split("\t")
+            self.experimental_dict[expcor[0]] = float(expcor[1])
+            
+
     def get_pearson_correlation(self, subset, subsetid, scores):
         array1 = list()
         array2 = list()
 
 
-    
+
 
 A = Analysis("/Users/brianmendoza/Dropbox/Rosetta/TrimmedScores/","4UN4")
 
