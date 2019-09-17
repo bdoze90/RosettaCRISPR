1b45733c025b0c4fbbe5e0453e964fea5894f243
bdoze90 <bjmendo@gmail.com>
2019-09-17 10:04:55 -0400

    Changes to the Analysis package to evaluate ddG values

---
+++ TestRefPass.py
@@ -0,0 +1,15 @@
+"""Testing file checking how to pass by reference to class objects"""
+
+import os
+import random
+
+"""class Qtest:
+
+    a = [5, 10, 12, 30]
+    b = [3, 7, 2, 15]
+    for i in range(len(a)):
+        print(a[i]-b[i])"""
+
+
+#Q = Qtest()
+print(random.randint(1,2))
